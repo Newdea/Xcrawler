cc0625dc1643a0512001cdcb8041016530c6fc01
jiuling0 <fengzhengxia@gmail.com>
2018-06-08 00:44:02 +0800

    get_list is OK

--- easy_1_get_html.py
+++ easy_1_get_html.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3          # 标准注释
 # -*- coding:utf-8 -*-          # 表示.py文件本身使用标准UTF-8编码
 
-"""a test hello module"""  # 表示模块的文档注释，任何模块代码的第一个字符串都被视为模块的文档注释
+"""a sample of web scrapy."""  # 表示模块的文档注释，任何模块代码的第一个字符串都被视为模块的文档注释
 
 __author__ = 'Newdea'  # 变量把作者写进去，这样当你公开源代码后别人就可以瞻仰你的大名
 
@@ -11,11 +11,17 @@ from bs4 import BeautifulSoup
 html = urlopen('https://wap.baidu.com/')        # open url ,get the content
 # send it to bs4_obj
 bs4_obj = BeautifulSoup(html.read(),'html.parser')
-# find all class='ns-text' 的span 标签
-text_list = list(BeautifulSoup.find_all("span", "ns-text"))
+# find all class='ns-text' 的span 标签"span",
+tag = bs4_obj.new_tag("span")
+bs4_obj.pushTag(tag)
+# todo: it's empty,can't use
+text_list = bs4_obj.find_all(tag,attrs={'class': "ns-text"})
+
 for text in text_list:
-    print(text.get_text())
+    print('%s' % text)
 html.close()
+
+
 
 
 
--- easy_1_get_list.py
+++ easy_1_get_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3          # 标准注释
 # -*- coding:utf-8 -*-          # 表示.py文件本身使用标准UTF-8编码
 
-"""get dynamic webpage content"""  # 表示模块的文档注释，任何模块代码的第一个字符串都被视为模块的文档注释
+"""get dynamic webpage content. run good ~"""  # 表示模块的文档注释，任何模块代码的第一个字符串都被视为模块的文档注释
 
 __author__ = 'Newdea'  # 变量把作者写进去，这样当你公开源代码后别人就可以瞻仰你的大名
 
